fa4d25e368e56b39d1a73b2642695b5298bc59e6
Lucas Rumney <lucasrumney94@gmail.com>
2018-02-08 20:57:29 -0800

    add flight scene

---
+++ Art/Assets/Lifetime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour {
+	public float lifetimeInSeconds;
+
+	private float initTime;
+	// Use this for initialization
+	void Start () 
+	{
+		initTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Time.time - initTime > lifetimeInSeconds)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Art/Assets/script/birdBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdBehavior : MonoBehaviour {
+
+	public GameObject streak;
+	private Vector3 newPosition;
+	private Vector3 wiggle;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt(Vector3.zero);
+		newPosition = new Vector3(Mathf.Sin(Time.time),Mathf.Cos(Time.time), 0.0f);
+		newPosition *= 5.0f;
+		wiggle = new Vector3(2.0f*Mathf.Sin(4*Time.deltaTime),2.0f*Mathf.Cos(4*Time.deltaTime),0.0f);
+		newPosition += new Vector3(wiggle.x*transform.right.x,wiggle.y*transform.right.y,0.0f);
+		newPosition += new Vector3(wiggle.x*transform.up.x,wiggle.y*transform.up.y,0.0f);
+		transform.position = newPosition;
+
+
+		if (Random.Range(1,100)%50 == 0)
+		{
+			GameObject.Instantiate(streak, transform.position, Quaternion.identity);
+		}
+
+	}
+}
